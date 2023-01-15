9b82c13b17d9e2ec7f18ff02bc02a4d9e7480589
Trinh Kim Tuan <kimtuan1102@gmail.com>
2023-01-15 16:48:22 +0700

    change to native mongo

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 3000;
 const bodyParser= require('body-parser');
 const mongoose = require('mongoose')
 const csv = require("./controller");
-
+const { MongoClient } = require('mongodb');
 mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(()=> {
         console.log('Database connected');
--- controller.js
+++ controller.js
@@ -1,10 +1,21 @@
 const Course = require('./course')
 const fastCsv = require('fast-csv')
 const fs = require("fs");
+const {MongoClient} = require("mongodb");
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+client.connect().then(()=> {
+    console.log('Database connected');
+})
+    .catch((error)=> {
+        console.log(error)
+        console.log('Error connecting to database');
+    });
+const courses = client.db().collection('courses');
 function csv(req, res) {
 
-    const cursor = Course.find({}).lean().cursor({batchSize: 1e6});
-    console.time("start")
+    const cursor = courses.find({}).stream()
+    const start = Date.now()
 
     const transformer = (doc)=> {
         return {
@@ -15,14 +26,14 @@ function csv(req, res) {
     }
 
     const filename = 'export.csv';
-    var stream = fs.createWriteStream('myFile.csv', {flags: 'a'});
+    var stream = fs.createWriteStream('myFile.csv', {highWaterMark: 32});
     res.setHeader('Content-disposition', `attachment; filename=${filename}`);
     res.writeHead(200, { 'Content-Type': 'text/csv' });
 
     res.flushHeaders();
 
     var csvStream = fastCsv.format({headers: true}).transform(transformer)
-    cursor.pipe(csvStream).pipe(stream);
-    console.time("end")
+    cursor.pipe(csvStream).pipe(res);
+    res.on('finish', () => console.log("Elapse Time: ", Date.now() - start))
 }
 module.exports = csv
